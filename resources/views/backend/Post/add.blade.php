cda634255143ad75cdba5ac0d2f8267aa4c8d9df
canhoduclong <canhoduclong@gmail.com>
2020-08-04 22:07:26 +0700

    upload 4/8

--- resources/views/backend/Post/add.blade.php
+++ resources/views/backend/Post/add.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 <form method="post" enctype="multipart/form-data">
 	@csrf
+	<p>category: </p>
 	<p>title: <input type="text" name="title" value="{{ old('title') ?? $category->title ?? '' }}"></p>
 	<p>keyword: <input type="text" name="keyword" value="{{ old('keyword') ?? $category->keyword ?? '' }}"></p>
 	<p>description:<input type="text" name="description"value="{{ old('description') ?? $category->description ?? '' }}"></p>
